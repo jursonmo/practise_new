751e9895c10c94250056f8d34a68b59afc3f9347
william <myname@example.com>
2025-01-04 00:36:47 +0800

    更新topicservice example/main.go

--- pkg/topicservice/example/main.go
+++ pkg/topicservice/example/main.go
@@ -2,52 +2,54 @@ package main
 
 import (
 	"context"
-	"time"
+	"os"
 
 	topicservice "github.com/jursonmo/practise_new/pkg/topicservice"
 	"github.com/zeromicro/go-zero/core/discov"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
-func main() {
-	sc1 := &topicservice.ServiceConfig{
+// ./main 1 false
+// ./main 2 true //看2号服务能否抢占成为leader
+func init() {
+	logx.DisableStat()
+}
+
+func StartService(id string, isLeader bool) {
+	sc := &topicservice.ServiceConfig{
 		Name:      "topic_service",
-		Id:        "1",
+		Id:        id,
 		Endpoints: []string{"tcp://127.0.0.1:8080"},
 
 		Etcd: discov.EtcdConf{
 			Hosts: []string{"127.0.0.1:2379"},
-			Key:   "topic_key",
+			Key:   "services",
 		},
+		IsLeader: isLeader,
 	}
-	service1, err := topicservice.NewService(sc1)
-	if err != nil {
-		logx.Error(err)
-		return
-	}
-	sc2 := &topicservice.ServiceConfig{
-		Name:      "topic_service",
-		Id:        "2",
-		IsLeader:  true,
-		Endpoints: []string{"tcp://127.0.0.1:8080"},
-		Etcd: discov.EtcdConf{
-			Hosts: []string{"127.0.0.1:2379"},
-			Key:   "topic_key",
-		},
-	}
-	service2, err := topicservice.NewService(sc2)
+	service, err := topicservice.NewService(sc)
 	if err != nil {
 		logx.Error(err)
 		return
 	}
 	topics := []string{"topic1", "topic2", "topic3", "topic4", "topic5", "topic6", "topic7", "topic8", "topic9", "topic10"}
-	service1.SetTopics(topics)
-	service2.SetTopics(topics)
+	service.SetTopics(topics)
 
-	logx.Info("start service1")
-	service1.Start(context.Background())
-	time.Sleep(time.Second)
-	logx.Info("start service2")
-	service2.Start(context.Background())
+	err = service.Start(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	logx.Infof("service started, id:%s, isLeader:%v", id, isLeader)
 	select {}
+}
+func main() {
+	if len(os.Args) < 2 {
+		logx.Errorf("Usage: %s <id> <isLeader>", os.Args[0])
+		return
+	}
+	isLeader := false
+	if len(os.Args) > 2 {
+		isLeader = true
+	}
+	StartService(os.Args[1], isLeader)
 }
