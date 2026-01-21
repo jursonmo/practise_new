7991035681721a292f86d6a90578c2a865116e66
william <myname@example.com>
2026-01-21 22:58:02 +0800

    打印函数调用栈,方便查看函数的调用过程

---
+++ cmd/call_stack/call_stack.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf("  %s\n", "123"))
+	builder.WriteString(fmt.Sprintf("  %s\n", "456"))
+	fmt.Println(builder.String())
+	test1()
+}
+func test1() {
+	fmt.Println("test1")
+	test2()
+}
+func test2() {
+	fmt.Println(GetCallStack(2))
+}
+
+// 获取当前调用栈的字符串表示
+func GetCallStack(skip int) string {
+	var pc [50]uintptr
+	n := runtime.Callers(skip, pc[:])
+
+	var builder strings.Builder
+	builder.WriteString("调用栈:\n")
+
+	for i := 0; i < n; i++ {
+		f := runtime.FuncForPC(pc[i])
+		file, line := f.FileLine(pc[i])
+
+		// 格式化输出
+		// 可以过滤掉runtime包的内容
+		if !strings.Contains(f.Name(), "runtime.") || !strings.Contains(f.Name(), "runtime/internal") {
+			builder.WriteString(fmt.Sprintf("  %s\n    %s:%d\n",
+				f.Name(), file, line))
+		}
+	}
+	return builder.String()
+}
